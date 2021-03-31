5e82c0709b2cc896c7286c6c8a1efe8326e31fd3
VedaantKuchhal <vedaantk22@gmail.com>
2021-03-30 21:46:59 -0400

    Adding DataFrame converter.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import pandas as pd
 from get_songids import get_playlist_tracks
 from track_list_feature import id_to_duration
 track_id_list = get_playlist_tracks('5FN6Ego7eLX6zHuCMovIR2')
-print(id_to_duration(track_id_list))
+track_stuff = id_to_duration(track_id_list)
+
+panda_data = pd.DataFrame.from_dict(track_stuff)
+panda_data_clean = pd.json_normalize(panda_data['audio_features'])
