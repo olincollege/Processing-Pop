712c644884261b2ac59e635651c94d215435afde
benji <pughbenji@gmail.com>
2021-04-03 18:18:05 -0400

    Fixed Column Naming
    Added Artist column

--- get_billboard.py
+++ get_billboard.py
@@ -6,7 +6,7 @@ def hot_100_data(year_start, year_end):
     # haha you think I'd be responsible and add a docstring? LOSER!
     # TODO: change this
     
-    all_hot_100 = pd.DataFrame(columns=["Date", "Song"])
+    all_hot_100 = pd.DataFrame(columns=["Date", "Song", "Artist"])
 
     for year in range(year_start, year_end, 1):
 
@@ -15,14 +15,6 @@ def hot_100_data(year_start, year_end):
             current_chart = \
             billboard.ChartData("hot-100", date=current_date).entries
             for song in current_chart:
-                all_hot_100 = all_hot_100.append({"Date": current_date, "Song": f"{song.title} {song.artist}"}, ignore_index=True)
+                all_hot_100 = all_hot_100.append({"Date": current_date, "Song": song.title, "Artist": song.artist}, ignore_index=True)
         
     return all_hot_100
-    
-
-
-def convert_chart(current_chart):
-    chart_list = []
-    for chart_entry in current_chart:
-        chart_list.append({"title": chart_entry.title, "artist": chart_entry.artist})
-    return chart_list
