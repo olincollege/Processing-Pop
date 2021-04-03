60afc18a890fe98319271ed5e5c2d25f0d0574f5
VedaantKuchhal <vedaantk22@gmail.com>
2021-04-03 18:39:54 -0400

    Final version of final_billboard.

--- final_billboard.py
+++ final_billboard.py
@@ -14,7 +14,7 @@ import datetime
 Return the Billboard Hot Hundred playlists for each year within the specified
 start and end years.
 
-Loop through the first day of the January for each year of the specified time
+Loop through the first day of June for each year of the specified time
 period, and extract the list of Billboard Hot Hundred songs on that date. This
 list is extracted by using the python library 'billboard'. All the various list
 of Hot Hundred songs for each year are then returned as one big dataframe.
@@ -28,50 +28,21 @@ Args:
     which the Billboard Hot 100 charts are queried.
 
 Returns:
-    A pandas dataframe where each column contains the Billboard Hot Hundred
-    chart for a particular date (in this case, the first day of each month in
-    the specified time period).
+    A pandas dataframe containing columns with the Date, Song, and Artist for
+    every Billboard Hot 100 Song on June 1st of the years specified.
 
 """
 def hot_100_data(year_start, year_end):
 
-    # Declare an empty dictionary that will store all the data.
-    hot_100_by_date_dict = {}
 
-    # Loop through every month of every year in the specified range.
-    for year in range(year_start, year_end + 1):
-        current_date = datetime.date(year, 1, 1)
+    all_hot_100 = pd.DataFrame(columns=["Date", "Song", "Artist"])
 
-        # Extract the Billboard Hot Hundred chart for the specific date.
+    for year in range(year_start, year_end + 1, 1):
+        current_date = datetime.date(year, 6, 1)
         current_chart = \
         billboard.ChartData("hot-100", date=current_date).entries
+        for song in current_chart:
+            all_hot_100 = all_hot_100.append({"Date": current_date, \
+            "Song": song.title, "Artist": song.artist}, ignore_index=True)
 
-        # Convert the data into a more readable format
-        hot_100_by_date_dict[current_date] = convert_chart(current_chart)
-    hot_100_by_date = pd.DataFrame(hot_100_by_date_dict)
-    return hot_100_by_date
-
-
-"""
-A helper function to hot_100_data. Return a Billboard Hot Hundred chart as a
-list.
-
-Convert the data in each track from a billboard library-specific 'track object'
-to a dictionary containing the track's title and artist.
-
-Args:
-    current_chart: A list of 'track objects'.
-
-Returns:
-    A list of dictionaries, each dictionary containing a track's title and
-    artist.
-"""
-def convert_chart(current_chart):
-
-    # Declare an empty list that will store the data for that chart.
-    chart_list = []
-
-    # Loop through each entry, and convert it into a dictionary.
-    for chart_entry in current_chart:
-        chart_list.append({"title": chart_entry.title, "artist": chart_entry.artist})
-    return chart_list
+    return all_hot_100
