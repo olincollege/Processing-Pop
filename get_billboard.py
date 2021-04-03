1b09d723a59e8b91cd7ecbf4fb266fa1d7387acd
benji <pughbenji@gmail.com>
2021-04-03 18:09:49 -0400

    Changed dataframe format
    Uses Date, Song and Artist columns instead of date columns singularly

--- get_billboard.py
+++ get_billboard.py
@@ -6,16 +6,18 @@ def hot_100_data(year_start, year_end):
     # haha you think I'd be responsible and add a docstring? LOSER!
     # TODO: change this
     
-    hot_100_by_date = {}    
+    all_hot_100 = pd.DataFrame(columns=["Date", "Song"])
 
     for year in range(year_start, year_end, 1):
+
         for month in [i + 1  for i in range(12)]:
             current_date = datetime.date(year, month, 1)
             current_chart = \
             billboard.ChartData("hot-100", date=current_date).entries
-            hot_100_by_date[current_date] = convert_chart(current_chart)
+            for song in current_chart:
+                all_hot_100 = all_hot_100.append({"Date": current_date, "Song": f"{song.title} {song.artist}"}, ignore_index=True)
         
-    return hot_100_by_date
+    return all_hot_100
     
 
 
