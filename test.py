7eaf52657c21b89cb85b74357bd207c0b515c9a7
benji <pughbenji@gmail.com>
2021-04-08 22:33:09 -0400

    Added tests for averae_all()
    TODO: fix not working functions in average all

--- test.py
+++ test.py
@@ -9,8 +9,8 @@ from spotify_id_query import (
 )
 
 from music_feature import (
-    average_by_date
-    average_all
+    average_by_date,
+    average_all,
     key_proportion
 )
 
@@ -46,37 +46,98 @@ query_track_cases = [
     ("asdjflasjdf", "Lil Nas X", "Error: Not in Spotify")
 ]
 
-# Construct test DataFrames for average_by_date(), average_all(), and key_proportion()
-test_dict_1 = {"Date": ["6/1/2020"], "Loudness": [0.1]}
-test_dataframe_1 = pd.DataFrame(test_average_dict)
-single_average_dict_1 = {"Date": [datetime.date(6,1,2020)], "Feature": ["Loudness"], "Average": [0.1], }
+# Construct test DataFrames for average_by_date()
+test_dict_1 = {"Date": [str(datetime.date(2020, 6, 1))], "Loudness": [0.1]}
+test_dataframe_1 = pd.DataFrame(test_dict_1)
+single_average_dict_1 = {"Date": [datetime.date(2020, 6, 1)], "Feature": ["Loudness"], "Average": [0.1], }
 single_average_dataframe_1 = pd.DataFrame(single_average_dict_1)
 
-test_dict_2 = {"Date": ["6/1/2020", "6/1/2020"], "Loudness": [0.1, 0.3]}
-test_dataframe_1 = pd.DataFrame(test_average_dict)
-single_average_dict_1 = {"Date": [datetime.date(6,1,2020)], "Feature": ["Loudness"], "Average": [0.2], }
-single_average_dataframe_1 = pd.DataFrame(single_average_dict_1)
+test_dict_2 = {"Date": [str(datetime.date(2020, 6, 1)), str(datetime.date(2020, 6, 1))], "Loudness": [0.1, 0.3]}
+test_dataframe_2 = pd.DataFrame(test_dict_2)
+single_average_dict_2 = {"Date": [datetime.date(2020, 6, 1)], "Feature": ["Loudness"], "Average": [0.2], }
+single_average_dataframe_2 = pd.DataFrame(single_average_dict_2)
 
-test_dict_1 = {"Date": ["6/1/2020", "6/1/2019"], "Loudness": [0.1, 0.3]}
-test_dataframe_1 = pd.DataFrame(test_average_dict)
-single_average_dict_1 = {"Date": [datetime.date(6,1,2019), datetime.date(6,1,2020)], "Feature": ["Loudness", "Loudness"], "Average": [0.1, 0.3]}
-single_average_dataframe_1 = pd.DataFrame(single_average_dict_1)
+test_dict_3 = {"Date": [str(datetime.date(2020, 6, 1)), str(datetime.date(2019, 6, 1))], "Loudness": [0.1, 0.3]}
+test_dataframe_3 = pd.DataFrame(test_dict_3)
+single_average_dict_3 = {"Date": [datetime.date(2019, 6, 1), datetime.date(2020, 6, 1)], "Feature": ["Loudness", "Loudness"], "Average": [0.3, 0.1]}
+single_average_dataframe_3 = pd.DataFrame(single_average_dict_3)
+
+test_dict_4 = {"Date": [str(datetime.date(2020, 6, 1)), str(datetime.date(2019, 6, 1))], "Loudness": [0.4, 0.5]}
+test_dataframe_4 = pd.DataFrame(test_dict_4)
+single_average_dict_4 = {"Date": [datetime.date(2020, 6, 1)], "Feature": ["Loudness"], "Average": [0.4]}
+single_average_dataframe_4 = pd.DataFrame(single_average_dict_4)
+
+test_dict_5 = {"Date": [str(datetime.date(2020, 6, 1)),\
+str(datetime.date(2020, 6, 1)), str(datetime.date(2019, 6, 1)),\
+str(datetime.date(2019, 6, 1))], "Loudness": [0.4, 0.6, 0.3, 0.5]}
+test_dataframe_5 = pd.DataFrame(test_dict_5)
+single_average_dict_5 = {"Date": [datetime.date(2019, 6, 1),\
+datetime.date(2020, 6, 1)], "Feature": ["Loudness", "Loudness"], "Average": [0.4,0.5]}
+single_average_dataframe_5 = pd.DataFrame(single_average_dict_5)
 
 average_by_date_cases = [
+    # Cannot take average of no numbers (and is outlined in Docstring), so no
+    # testing empty dataframes.
     # Test that a single year, single item dataframe yields the correct average
-    (2020, 2020, "Loudness", test_dataframe_1, single_average_dataframe_1)
+    (2020, 2020, "Loudness", test_dataframe_1, single_average_dataframe_1),
     # Test that a single year, multiple item dataframe yields the correct average
-    (2020, 2020, "Loudness", test_dataframe_2, single_average_dataframe_2)
-    # Test that a multiple year, single item dataframe yields correct average
-    (2019, 2020, "Loudness", test_dataframe_2, single_average_dataframe_2)
+    (2020, 2020, "Loudness", test_dataframe_2, single_average_dataframe_2),
+    # Test that a multiple year, single item dataframe yields correct averages
+    (2019, 2020, "Loudness", test_dataframe_3, single_average_dataframe_3),
+    # Test that limiting the range of years limits the results in a multi year dataframe
+    (2020, 2020, "Loudness", test_dataframe_4, single_average_dataframe_4),
+    # Test that a multiple year, multiple item dataframe yields correct averages
+    (2019, 2020, "Loudness", test_dataframe_5, single_average_dataframe_5)
 ]
 
+# Construct test DataFrames for average_by_date()
+test_dict_6 = {"Date": [str(datetime.date(2020, 6, 1))], "Loudness": [0.1], "Duration": [5.0]}
+test_dataframe_6 = pd.DataFrame(test_dict_6)
+multi_average_dict_6 = {"Date": [datetime.date(2020, 6, 1), \
+datetime.date(2020, 6, 1)], "Feature": ["Loudness", "Duration"], "Average": [0.1, 5.0]}
+multi_average_dataframe_6 = pd.DataFrame(multi_average_dict_6)
+
+test_dict_7 = {"Date": [str(datetime.date(2019, 6, 1)), str(datetime.date(2019, 6, 1)), str(datetime.date(2020, 6, 1)), str(datetime.date(2020, 6, 1))], "Loudness": [0.1, 0.3, 0.8, 1.0], "Duration": [5.0, 7.0, 30.0, 40.0]}
+test_dataframe_7 = pd.DataFrame(test_dict_7)
+multi_average_dict_7 = {"Date": [datetime.date(2019, 6, 1), datetime.date(2020, 6, 1), datetime.date(2019, 6, 1), datetime.date(2020, 6, 1)], "Feature": ["Loudness", "Duration"], "Average": [0.2, 0.9, 6.0, 35.0]}
+multi_average_dataframe_7 = pd.DataFrame(multi_average_dict_6)
+
+average_all_cases = [
+    # Test that a single year, single feature, single item dataframe yields the
+    # correct average
+    (2020, 2020, ["Loudness"], test_dataframe_1, single_average_dataframe_1),
+    # Test that a single year, multiple feature, singe item dataframe yields
+    # the correct averages
+    (2020, 2020, ["Loudness", "Duration"], test_dataframe_6, multi_average_dataframe_6),
+    # Test that multiple year, multiple feature, multiple item dataframe yields
+    (2020, 2020, ["Loudness", "Duration"], test_dataframe_7, multi_average_dataframe_7),
+    # Since this function calls average_by_date, there isn't very much need to test
+    # the specifics of individual behaviors.
+]
+
+# Construct test DataFrames for key_proportion()
+# test_dict_8 = {"Date": [str(datetime.date(2020, 6, 1))], "Key": ["A"]}
+# test_dataframe_8 = pd.DataFrame(test_dict_8)
+# single_average_dict_8 = {"Date": [datetime.date(2020, 6, 1)], "Key": ["A"], "Count": [1], "Proportion":}
+# single_average_dataframe_8 = pd.DataFrame(single_average_dict_8)
+
+# Test clean_artist()
 @pytest.mark.parametrize("artist,cleaned_artist", clean_artist_cases)
 def test_clean_artist(artist, cleaned_artist):
     assert clean_artist(artist) == cleaned_artist
 
-
+# Test query_track()
 @pytest.mark.parametrize("title,artist,track_id", query_track_cases)
 def test_query_track(title, artist, track_id):
     assert query_track(title, artist) == track_id
+
+# Test average_by_date
+@pytest.mark.parametrize("start_date,end_date,feature,song_dataframe,date_average", average_by_date_cases)
+def test_average_by_date(start_date, end_date, feature, song_dataframe, date_average):
+    assert average_by_date(start_date, end_date, feature, song_dataframe).equals(date_average)
+
+# Test average_all
+@pytest.mark.parametrize("start_date,end_date,features,song_dataframe,date_average", average_all_cases)
+def test_average_all(start_date, end_date, features, song_dataframe, date_average):
+    assert average_all(start_date, end_date, features, song_dataframe).equals(date_average)
 
