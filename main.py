a24324df7c0caccbd1b1e4f13bf168b639c0215c
VedaantKuchhal <vedaantk22@gmail.com>
2021-04-09 01:55:43 -0400

    Modified main.py for testing.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import billboard_scraper
 import spotify_id_query
 import get_audio_features
 
-billboard_data = billboard_scraper.hot_100_data(1991,2000)
+billboard_data = billboard_scraper.hot_100_data(1961,2020)
 id_data = spotify_id_query.query_all_tracks(billboard_data)
 audio_data = get_audio_features.find_audio_features(id_data)
 
-audio_data.to_csv(path_or_buf='1990s.csv')
+print(audio_data)
